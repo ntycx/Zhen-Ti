45bd3fd6c1fb3c0412a90cc9d5f50b4d36137cea
ntycx <54228900+ntycx@users.noreply.github.com>
2019-11-15 17:57:19 +0800

    Update 2005-3.cpp

--- 2005-3.cpp
+++ 2005-3.cpp
@@ -7,16 +7,22 @@
 #include<math.h>
 using namespace std;
 struct Farction{
-	int up;//分子 
-	int down;//分母 
+	long long up;//分子 
+	long long down;//分母 
 }f1,f2,sum;
 int main(){
 	f1.down=1;
-	f2.up=2;
-	for(int i=0;i<19;i++){
+	f1.up=2;
+	sum.up=f1.up;
+	sum.down=f1.down;//初始化sum 
+	for(int i=0;i<19;i++){//从第1个加到20个，共加19次 
 		f2.up=f1.down+f2.up;
 		f2.down=sum.up;
-		sum.up=f1.up*f2.down+f1.down*f2.up;
-		sum.down=f1.down*f2.down;
+		sum.up=sum.up*f2.down+sum.down*f2.up;
+		sum.down=sum.down*f2.down;//分数加法
+		f1.up=f2.up;
+		f1.down=f2.down; 
 	}
+	printf("结果为：%lld/%lld",sum.up,sum.down);
+	return 0;
 } 
