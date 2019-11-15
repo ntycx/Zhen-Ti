fe9220263d5933d8050330b3576b59250bb86948
ntycx <54228900+ntycx@users.noreply.github.com>
2019-11-15 16:22:58 +0800

    Update 2005-2.cpp

--- 2005-2.cpp
+++ 2005-2.cpp
@@ -6,11 +6,10 @@
 #include<algorithm>
 #include<math.h>
 using namespace std;
-const int INF=1000000000;
 int main(){
 	int a[5][5];
 	int max,row,col;
-	bool flag=false;
+	bool flag=false; //标记是否找到鞍点 
 	printf("请输入一个5行5列的矩阵:\n");
 	for(int i=0;i<5;i++){
 		for(int j=0;j<5;j++){
@@ -19,21 +18,22 @@ int main(){
 	}
 	for(int i=0;i<5;i++){
 		max=0;
-		for(int j=0;j<5;j++){
+		for(int j=0;j<5;j++){//找到每一行的最大值 
 			if(a[i][j]>max){
 				max=a[i][j];
 				row=i;
 				col=j;
 			}
 		}
-		for(int k=0;k<5;k++){
-			if(a[k][col]<max) break;
+		for(int k=0;k<5;k++){//比较最大值所在列的所有元素 
+			if(a[k][col]<max) break;//如果不是列的最小值，跳出循环 
 			else{
+				flag=true;
 				printf("鞍点为:a[%d][%d]=%d",row,col,a[row][col]);
-				return 0;
+				break;
 			}
 		}
 	}
-	printf("没有鞍点");
+	if(flag==false)printf("没有鞍点");
 	return 0;
 }
