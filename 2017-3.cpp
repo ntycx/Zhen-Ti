3d1db203af1fa9e3fc60f406762abd1d8dad8ae9
ntycx <54228900+ntycx@users.noreply.github.com>
2019-12-02 19:07:58 +0800

    Update 2017-3.cpp

--- 2017-3.cpp
+++ 2017-3.cpp
@@ -6,7 +6,7 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-const int INF=1000000000;
+const int INF=1000000000;//无限大
 int main(){
 	vector<int> num;//向量num存储符合条件的值 
 	vector<int> resRow;//向量resRow存储符合条件的值对应的行 
@@ -41,6 +41,7 @@ int main(){
 		}
 	}
 	int lenth=num.size();//总共有多少个点
+	if(lenth==0)printf("NO FIND!\n");//长度为0则没找到对应元素 
 	for(int i=0;i<lenth-1;i++){
 		printf("%d(%d,%d)",num[i],resRow[i],resCol[i]);
 		if(i<lenth-1)printf("，");
